d8e3a8b38f573194212152b0f44710ed8fa29bdf
Md Shere Habib <145744596+idfcLeo@users.noreply.github.com>
2026-07-18 10:30:02 +0530

    Time: 0 ms (100%), Space: 48.5 MB (14.14%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,12 +1,18 @@
 class Solution {
     public int search(int[] nums, int target) {
-        HashMap<Integer,Integer>map=new HashMap<>();
-        int n = nums.length;
-        for(int i=0;i<n;i++){
-            map.put(nums[i],i);
-        }
-        if(map.containsKey(target)){
-            return map.get(target);
+        int low=0;
+        int high= nums.length-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(target<nums[mid]){
+                high=mid-1;
+            }
+            else {
+                low= mid+1;
+            }
         }
         return -1;
     }
