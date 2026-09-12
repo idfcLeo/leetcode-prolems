1af48244b77dca946a34adde0273d50ecde5f20b
Md Shere Habib <145744596+idfcLeo@users.noreply.github.com>
2026-09-12 15:58:07 +0530

    Time: 2 ms (6.41%), Space: 50.6 MB (5.4%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,12 +1,13 @@
-class Solution{
-    public void reverseString(char[]s){
-        int left=0, right= s.length-1;
-        while(left<right){
-            char temp=s[left];
-            s[left]=s[right];
-            s[right]=temp;
-            left++;
-            right--;
+class Solution {
+    public void reverseString(char[] s) {
+        int n = s.length;
+        ArrayList<Character>list= new ArrayList<>();
+        for(int i=0;i<n;i++){
+            list.add(s[i]);
         }
+        Collections.reverse(list);
+        for(int i=0;i<s.length;i++){
+            s[i]=list.get(i);
+        }        
     }
 }
