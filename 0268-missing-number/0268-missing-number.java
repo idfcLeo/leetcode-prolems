63583c3f85c6c40ea3af64bae0ba92d0d2146283
Md Shere Habib <145744596+idfcLeo@users.noreply.github.com>
2026-09-12 13:31:36 +0530

    Time: 0 ms (100%), Space: 47.3 MB (48.75%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int sum=0;
+        
+        int sum2=0;
+        for(int i =1;i<=nums.length;i++){
+            sum2+=i;
+        }
+        for(int i =0;i<nums.length;i++){
+            sum=sum+nums[i];
+        }
+        return sum2-sum;
+    }
+}
